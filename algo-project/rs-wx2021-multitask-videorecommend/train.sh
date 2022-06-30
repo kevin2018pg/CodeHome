ebf7f5d2e91309a5af305e28d61eda09e56f5793
chendong14776 <chendong14776@autohome.com.cn>
2022-06-30 14:37:03 +0800

    结果推理

---
+++ algo-project/rs-wx2021-multitask-videorecommend/train.sh
@@ -0,0 +1,13 @@
+# 特征处理
+cd ./src/prepare/
+python text_tfidf_feature.py && \
+python text_cluster_feature.py && \
+python feed_user_author_w2v_feature.py && \
+
+# 生成训练数据
+cd ./src/train/
+python generate_train_dat.py && \
+
+# 训练最终模型
+python model_train.py
+
